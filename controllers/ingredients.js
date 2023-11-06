348686ebeb58086cfde79200aabba7d8cc82ddb4
Mrs_N <worksmart377@gmail.com>
2023-11-06 15:16:29 -0600

    WIP: adding ingredients quantity and unit of measure

--- controllers/ingredients.js
+++ controllers/ingredients.js
@@ -1,41 +1,61 @@
 const Recipe = require("../models/recipe");
 const Ingredient = require("../models/ingredient");
 
+// Function to render the form for creating a new ingredient
 async function newIngredient(req, res) {
   try {
+    // Find all existing ingredients in the database
     const foundIngredient = await Ingredient.find({});
-    res.render("ingredients/new", {
-      title: "new ingredient",
-      ingredient: foundIngredient,
-    });
+      // Check if the ingredient already exists
+      const existingIngredient = await Ingredient.findOne({ foundIngredient });
+
+      if (existingIngredient) {
+        // Handle the case where the ingredient already exists
+        res.status(409).render("error", {
+          title: "Ingredient already exists",
+        });
+      } else {
+        const ingredientName = req.body.ingredientName; // assuming ingredientName is sent in the request body
+        // Insert the new ingredient
+        const newIngredient = new Ingredient({ ingredientName });
+        await newIngredient.save();
+  
+        // Redirect or render success view
+        res.redirect("/ingredients/new");
+      }
+    // Render the "new" view and pass in the found ingredients as data
+    res.render("ingredients/new");
   } catch (error) {
     console.log(error);
-    res.render("error", {
+    res.status(404).render("error", {
       title: "Something went wrong",
     });
   }
 }
 
-async function create(req, res) {
-  try {
-    //   req.body.ingredientName = req.body.ingredientName.trim(); // remove space chars at beginning or ending of string
-    //   req.body.ingredientName = req.body.ingredientName.split(/\s*,\s*/); // split comma seperated names into array
-
-    await Ingredient.create(req.body); //create method Ingredient Model is called with modified req.body object
-    //   const ingredientId = newIngredient._id; //object's id property is assigned to variable
-    //   const recipe = await Recipe.create(req.body); //create method Recipe Model is called with original req.body object
-    //   recipe.ingredients.push(ingredientId); //resulting recipe object's ingredients array is updated with IngredientId
-    //   await recipe.save(); //recipe object saved
-    res.redirect("/ingredients/new");
-  } catch (error) {
-    console.log(error);
-    res.render("error", {
-      title: "Something went wrong",
-    });
+  // Function to create a new ingredient
+  async function create(req, res) {
+    try {
+      const { ingredientName, ingredientQuantity, ingredientUOM } = req.body;
+      // Create a new ingredient using the data from the request body
+      await Ingredient.create({
+        ingredientName,
+        ingredientQuantity,
+        ingredientUOM
+      });
+      
+      // Redirect to the page for creating a new ingredient again
+      res.redirect("/ingredients/new");
+    } catch (error) {
+      console.log(error);
+      res.render("error", {
+        title: "Something went wrong",
+      });
+    }
   }
-}
 
 module.exports = {
   create,
   new: newIngredient,
 };
+``
--- models/ingredient.js
+++ models/ingredient.js
@@ -7,6 +7,13 @@ const ingredientSchema = new Schema(
     ingredientName: {
       type: String,
     },
+    ingredientQuantity: {
+      type: Number,
+    },
+    ingredientUOM: {
+      type: String,
+      enum: ["Each", "LB", "OZ", "Cup", "1/4 Cup", "1/3 Cup", "1/2 Cup", "TBSP", "1/4 TBSP", "1/3 TBSP", "1/2 TBSP", "TSP", "1/4 TSP", "1/3 TSP", "1/2 TSP","Pinch", "Can", "Package"],
+    },
   },
   { timestamps: true }
 );
